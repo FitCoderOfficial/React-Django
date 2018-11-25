116a20540e73afe32ebf448b4dcaead2504de1fe
SaeByeok Shin <toqur1219@naver.com>
2018-11-25 22:31:34 +0900

    feat: Django-allauth

--- deact-frontend/src/containers/Layout.js
+++ deact-frontend/src/containers/Layout.js
@@ -1,51 +1,64 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
+
 
 const { Header, Content, Footer } = Layout;
 
-const CustomLayout = (props) => {
-    return (
-        <Layout className="layout">
-            <Header>
-            <div className="logo" />
-            <Menu
-                theme="dark"
-                mode="horizontal"
-                defaultSelectedKeys={['2']}
-                style={{ lineHeight: '64px', textAlign: 'left' }}
-            >
+class CustomLayout extends React.Component {
+    render () {
+        return (
+            <Layout className="layout">
+                <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px', textAlign: 'left' }}
+                >
+    
+                {
+                    this.props.isAuthenticated ?
 
-            {
-                props.isAuthenticated ?
-                <Menu.Item key="2">
-                    로그아웃
-                </Menu.Item>
-                :
-                <Menu.Item key="2">
-                    <Link to='/login'>로그인</Link>
-                </Menu.Item>
-            }
-
-                <Menu.Item key="1">
-                    <Link to='/'>목록</Link>
-                </Menu.Item>
-            </Menu>
-            </Header>
-            <Content style={{ padding: '0 50px', textAlign: 'left' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item><Link to='/'>Home</Link></Breadcrumb.Item>
-                <Breadcrumb.Item><Link to='/'>List</Link></Breadcrumb.Item>
-            </Breadcrumb>
-                <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
-                    {props.children}
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©2018 Created by Ant UED
-            </Footer>
-        </Layout>
-    );
+                    <Menu.Item key="2" onClick={this.props.logout}>
+                        로그아웃
+                    </Menu.Item>
+                    :
+                    <Menu.Item key="2">
+                        <Link to='/login'>로그인</Link>
+                    </Menu.Item>
+                }
+    
+                    <Menu.Item key="1">
+                        <Link to='/'>목록</Link>
+                    </Menu.Item>
+                </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px', textAlign: 'left' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item><Link to='/'>Home</Link></Breadcrumb.Item>
+                    <Breadcrumb.Item><Link to='/'>List</Link></Breadcrumb.Item>
+                </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                        {this.props.children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
+        );
+    }    
 }
 
-export default CustomLayout;
+
+const mapDispathchToProps = dispatch => {
+    return {
+        logout: () => dispatch(actions.logout())
+    }
+}
+
+export default connect(null, mapDispathchToProps)(CustomLayout)
