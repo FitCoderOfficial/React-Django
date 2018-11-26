e07b149b0c576b6503ac4101dc95a505758d2cd5
SaeByeok Shin <toqur1219@naver.com>
2018-11-27 01:06:51 +0900

    refactor: Heroku setting
    Setting for heroku

--- deact-backend/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\works\pg_works\react\react-django\deact-backend\venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
